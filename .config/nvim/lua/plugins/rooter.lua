107111a150044a7a09ee8d333d3e061aec4bbe26
terryhsuu <b09902025@csie.ntu.edu.tw>
2025-03-06 10:40:33 +0800

    chore: change rooter s.t. it needs to be enabled via :Rooter

--- .config/nvim/lua/plugins/rooter.lua
+++ .config/nvim/lua/plugins/rooter.lua
@@ -1,9 +1,10 @@
 return {
--- 'airblade/vim-rooter'
   {
-      'notjedi/nvim-rooter.lua',
-      config = function()
-          require('nvim-rooter').setup()
-      end
-  },
+    'notjedi/nvim-rooter.lua',
+    config = function()
+      require('nvim-rooter').setup {
+        manual = true
+      }
+    end
+  }
 }
